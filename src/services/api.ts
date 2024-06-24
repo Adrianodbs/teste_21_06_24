4d5811e06f5c5d5715f4a396623840e7c516ce27
Adrianodbs <adriano_alves_2008@hotmail.com>
2024-06-24 18:37:22 -0300

    Fix: .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseURL = '/api';
-
+const baseURL = import.meta.env.NEXT_PUBLIC_API_URL || '/api';
 const apiData = axios.create({
   baseURL: baseURL
 })
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '/api',
+    },
+  },
   server: {
     proxy: {
       '/api': {
